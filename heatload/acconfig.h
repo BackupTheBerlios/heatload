ed9a5a229fadb277ba62884a0f6b7f358d9784b1
thoma <thoma>
2002-12-09 07:06:24 +0000

    ""

---
+++ heatload/acconfig.h
@@ -0,0 +1,13 @@
+// generated 2002/12/8 23:45:22 CET by thoma@Tiger.(none)
+// using glademm V1.1.2a_cvs
+
+/* acconfig.h
+ * This file is in the public domain.
+ * 
+ * Descriptive text for the C preprocessor macros that
+ * the distributed Autoconf macros can define.
+ * These entries are sometimes used by macros
+ * which glade-- uses.
+ */
+//#undef PACKAGE
+//#undef VERSION
